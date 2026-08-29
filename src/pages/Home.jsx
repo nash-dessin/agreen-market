573bb864f36346b50cca96c0da237a99a537761a
nash-dessin <n.whinga@gmail.com>
2026-08-30 00:14:08 +0300

    map out sections of the home page: hero, footer, header and featured sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+
+      <FeaturedProducts />
+
+      <FeaturedVendors />
+
+      <Footer />
+    </div>
+  );
+}

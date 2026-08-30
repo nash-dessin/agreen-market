843b25df2ad978841ea1f2b7814aa53e4ac72402
nash-dessin <n.whinga@gmail.com>
2026-08-30 09:55:22 +0300

    update home page to display featured goods and vendors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Hero from "../components/Hero";
+import FeaturedProducts from "../components/FeaturedProducts";
+import FeaturedVendors from "../components/FeaturedVendors";
+import Footer from "../components/Footer";
+
 export default function Home() {
   return (
     <div>

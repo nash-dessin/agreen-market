6ff3dd5da1484e72666a48e9ca8819de166c411a
nash-dessin <n.whinga@gmail.com>
2026-08-31 03:13:25 +0300

    rewrite routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductPage from "./pages/Product";
+import VendorsPage from "./pages/Vendors";
+import AboutPage from "./pages/About";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import NotFound from "./pages/NotFound";
+import AuthLogin from "./pages/AuthLogin";
+import AuthSignUp from "./pages/AuthSignUp";
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductPage />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app-shell">
+          <Navbar />
+          <main className="main-shell">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<ProductPage />} />
+              <Route path="/vendors" element={<VendorsPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/login" element={<AuthLogin />} />
+              <Route path="/signup" element={<AuthSignUp />} />
+              <Route path="/not-found" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </div>
+      </CartProvider>
     </BrowserRouter>
   );
 }
